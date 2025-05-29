144aa21f6a11947ce70f23739534f5194ee827ec
huanhust1998 <huanhust1998@gmail.com>
2025-05-29 22:09:27 +0700

    [Two Sum]: Method 1

--- two-sum.js
+++ two-sum.js
@@ -30,3 +30,25 @@ Constraints:
 -109 <= target <= 109
 Only one valid answer exists.
  */
+
+/**
+ * 
+ * @param {*} nums 
+ * @param {*} target 
+ * @returns 
+ * Cách này độ phức tạp thời gian của thuật toán là O(n2), độ phức tạp của không gian là O(1)
+ */
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+twoSum([2, 7, 11, 15], 9);
+twoSum([3, 2, 4], 6);
+twoSum([3, 3], 6);
+
+
